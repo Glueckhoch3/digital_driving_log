2290f041e262a858061c6398c30474a66b949f9f
Leonhard Schneider <lescit07@hs-esslingen.de>
2025-12-19 11:17:48 +0100

    initialize backend structure

---
+++ digidrivelog-backend/src/main/java/de/digidrivelog/DigidrivelogApplication.java
@@ -0,0 +1,13 @@
+package de.digidrivelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigidrivelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigidrivelogApplication.class, args);
+	}
+
+}
---
+++ digidrivelog-backend/src/test/java/de/digidrivelog/DigidrivelogApplicationTests.java
@@ -0,0 +1,13 @@
+package de.digidrivelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigidrivelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
