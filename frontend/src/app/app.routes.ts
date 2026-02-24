9d56d4438156107b0929a6c4ca1bc3e00b32c986
Leonhardit07 <lescit07@hs-esslingen.de>
2026-02-24 12:18:23 +0100

    Revert "Basic Frontend functionality"

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,20 +1,3 @@
 import { Routes } from '@angular/router';
-import { DrivingLogComponent } from './pages/driving-log/driving-log.component';
-import { DrivingHistoryComponent } from './pages/driving-history/driving-history.component';
-import { CostsComponent } from './pages/costs/costs.component';
-import { ShareholdersComponent } from './pages/shareholders/shareholders.component';
-import { SettlementsComponent } from './pages/settlements/settlements.component';
-import { FuelComponent } from './pages/fuel/fuel.component';
-import { ReportsComponent } from './pages/reports/reports.component';
 
-export const routes: Routes = [
-  { path: '', redirectTo: '/drives', pathMatch: 'full' },
-  { path: 'drives', component: DrivingLogComponent },
-  { path: 'history', component: DrivingHistoryComponent },
-  { path: 'costs', component: CostsComponent },
-  { path: 'shareholders', component: ShareholdersComponent },
-  { path: 'settlements', component: SettlementsComponent },
-  { path: 'fuel', component: FuelComponent },
-  { path: 'reports', component: ReportsComponent },
-  { path: '**', redirectTo: '/drives' }
-];
+export const routes: Routes = [];
