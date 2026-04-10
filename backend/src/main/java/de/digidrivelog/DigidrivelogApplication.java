458e9d9a894210bbe06d366b45f7467d9b8727c3
Leonhard Schneider <lescit07@hs-esslingen.de>
2026-04-10 19:21:36 +0200

    feat: add base path reply and resolve SonarQube issues.

--- backend/src/main/java/de/digidrivelog/DigidrivelogApplication.java
+++ backend/src/main/java/de/digidrivelog/DigidrivelogApplication.java
@@ -2,12 +2,21 @@ package de.digidrivelog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class DigidrivelogApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DigidrivelogApplication.class, args);
 	}
 
+	@RestController
+	class HelloController {
+		@GetMapping("/")
+		public String hello() {
+			return "Hello, Digidrivelog!";
+		}
+	}
 }
